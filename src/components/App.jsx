7f4308bb87b1cecb39194d3507321d3f1354e64a
Student <example@example.com>
2020-02-10 10:52:20 -0800

    add examples from homework

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList";
+import MyStyledComponent from "./MyStyledComponent";
+
+function App(){
+  return (
+    <div>
+      <Header/>
+      <TicketList/>
+      <MyStyledComponent/>
+    </div>
+  );
+}
+
+export default App;
